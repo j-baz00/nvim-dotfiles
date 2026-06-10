5f6a34e36b8d6750b7da7082869c0d3f1f515c9e
julian <jbaz@duck.com>
2026-06-10 17:00:08 -0400

    My first github push :)

---
+++ init.lua
@@ -0,0 +1,122 @@
+-- ~/.config/nvim/init.lua
+
+-- Basic setup
+vim.opt.nu = true -- line numbers
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.tabstop = 2 -- sets tab to 2 spaces
+vim.opt.shiftwidth = 2 -- number of spaces for each indentation
+vim.opt.expandtab = true -- spaces instead of tabs
+vim.opt.smartindent = true
+vim.opt.hlsearch = false -- highlight search results off
+vim.opt.softtabstop = 2
+
+-- sets <Space>w to save file
+vim.g.mapleader = ' '
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+
+-- LazyVim setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  "folke/tokyonight.nvim",
+  "nvim-tree/nvim-tree.lua",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup({
+        ensure_installed = { "lua", "python", "c", "cpp", "java" },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
+
+  { "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  { "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "clangd", "jdtls", "pyright" },
+        automatic_installation = true,
+      })
+    end
+  },
+  -- New LSP config using vim.lsp.config (nvim-lspconfig 1.0+ compatible with Neovim 0.11+)
+  { "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    config = function()
+      -- Lua LSP
+      vim.lsp.config.lua_ls = {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", ".git" },
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      }
+
+      -- C/C++ (clangd)
+      vim.lsp.config.clangd = {
+        cmd = { "clangd" },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+        root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "CMakeLists.txt", ".git" },
+      }
+
+      -- Java (jdtls)
+      vim.lsp.config.jdtls = {
+        cmd = { "jdtls" },
+        filetypes = { "java" },
+        root_markers = { "gradlew", "mvnw", "build.gradle", "pom.xml", ".git" },
+      }
+
+      -- Python (pyright)
+      vim.lsp.config.pyright = {
+        cmd = { "pyright-langserver", "--stdio" },
+        filetypes = { "python" },
+        root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+      }
+
+      -- Enable LSP servers
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("clangd")
+      vim.lsp.enable("jdtls")
+      vim.lsp.enable("pyright")
+    end,
+  },
+})
+
+-- Set colorscheme after plugins are loaded
+vim.cmd.colorscheme("tokyonight")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)  -- Auto-format file
+  end,
+})
